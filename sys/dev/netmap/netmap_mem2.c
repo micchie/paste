32405994816c1dd9e1d06e330fde2363657423a8
Michio Honda <micchie@sfc.wide.ad.jp>
2018-02-19 21:40:22 +0100

    increase limit

--- sys/dev/netmap/netmap_mem2.c
+++ sys/dev/netmap/netmap_mem2.c
@@ -443,14 +443,14 @@ struct netmap_mem_d nm_mem = {	/* Our memory allocator. */
 			.objminsize = sizeof(struct netmap_ring),
 			.objmaxsize = 32*PAGE_SIZE,
 			.nummin     = 2,
-			.nummax	    = 4096,
+			.nummax	    = 8256,
 		},
 		[NETMAP_BUF_POOL] = {
 			.name	= "netmap_buf",
 			.objminsize = 64,
 			.objmaxsize = 65536,
 			.nummin     = 4,
-			.nummax	    = 1000000, /* one million! */
+			.nummax	    = 3200000, /* one million! */
 		},
 	},
 
