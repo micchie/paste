luigirizzo/netmap

virt header for NICs

sys/dev/netmap/netmap_kern.h
@@ -1803,9 +1803,10 @@ PNMB(struct netmap_adapter *na, struct netmap_slot *slot, uint64_t *pp)
 	uint32_t i = slot->buf_idx;
 	struct lut_entry *lut = na->na_lut.lut;
 	void *ret = (i >= na->na_lut.objtotal) ? lut[0].vaddr : lut[i].vaddr;
+	u_int offset = na->virt_hdr_len;
 
 #ifndef _WIN32
-	*pp = (i >= na->na_lut.objtotal) ? lut[0].paddr : lut[i].paddr;
+	*pp = (i >= na->na_lut.objtotal) ? lut[0].paddr + offset : lut[i].paddr + offset;
 #else
 	*pp = (i >= na->na_lut.objtotal) ? (uint64_t)lut[0].paddr.QuadPart : (uint64_t)lut[i].paddr.QuadPart;
 #endif

micchie/paste

sys/dev/netmap/netmap_kern.h
@@ -2016,11 +2016,12 @@ PNMB(struct netmap_adapter *na, struct netmap_slot *slot, uint64_t *pp)
 	struct lut_entry *lut = na->na_lut.lut;
 	struct plut_entry *plut = na->na_lut.plut;
 	void *ret = (i >= na->na_lut.objtotal) ? lut[0].vaddr : lut[i].vaddr;
+	u_int offset = na->virt_hdr_len;
 
 #ifdef _WIN32
 	*pp = (i >= na->na_lut.objtotal) ? (uint64_t)plut[0].paddr.QuadPart : (uint64_t)plut[i].paddr.QuadPart;
 #else
-	*pp = (i >= na->na_lut.objtotal) ? plut[0].paddr : plut[i].paddr;
+	*pp = (i >= na->na_lut.objtotal) ? plut[0].paddr + offset : plut[i].paddr + offset;
 #endif
 	return ret;
 }