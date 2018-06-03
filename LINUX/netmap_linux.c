luigirizzo/netmap

stack: GONE flag to share bufs with stack more safely

LINUX/netmap_linux.c
@@ -831,6 +831,10 @@ nm_os_stmp_mbuf_data_destructor(struct ubuf_info *uarg,
 	struct nm_ubuf_info *u = (struct nm_ubuf_info *)uarg;
 
 	scb = container_of(u, struct stmp_cb, ui);
+	if (unlikely(stmp_cb_gone(scb))) {
+		stmp_cb_wstate(scb, SCB_M_NOREF); // XXX also clear GONE
+		return;
+	}
 	stmp_cb_wstate(scb, SCB_M_NOREF);
 	stmp_extra_dequeue(scb_kring(scb), scb_slot(scb));
 }

sys/dev/netmap/netmap_kern.h
@@ -1142,6 +1142,7 @@ struct stmp_cb {
 	struct nm_ubuf_info ui; /* ctx keeps kring and desc keeps slot */
 #define SCB_M_MAGIC		0x12345600	/* XXX do better */
 #define SCB_M_MAGIC_MASK	0xffffff00	/* XXX do better */
+#define SCB_GONE		0x00000001
 	uint32_t flags;
 	uint32_t next;
 } __attribute__((__packed__)); /* 32 byte */
@@ -1161,6 +1162,24 @@ static inline int
 stmp_cb_valid(struct stmp_cb *scb)
 {
 	return ((scb->flags & SCB_M_MAGIC_MASK) == SCB_M_MAGIC);
+}
+
+static inline int
+stmp_cb_gone(struct stmp_cb *scb)
+{
+	return !!(scb->flags & SCB_GONE);
+}
+
+static inline void
+stmp_cb_set_gone(struct stmp_cb *scb)
+{
+	scb->flags |= SCB_GONE;
+}
+
+static inline void
+stmp_cb_clr_gone(struct stmp_cb *scb)
+{
+	scb->flags &= ~SCB_GONE;
 }
 
 static inline int

micchie/paste

LINUX/netmap_linux.c
@@ -1043,6 +1043,10 @@ nm_os_st_mbuf_data_destructor(struct ubuf_info *uarg,
 	struct nm_ubuf_info *u = (struct nm_ubuf_info *)uarg;
 
 	scb = container_of(u, struct nm_st_cb, ui);
+	if (unlikely(nm_st_cb_gone(scb))) {
+		nm_st_cb_wstate(scb, SCB_M_NOREF); // XXX also clear GONE
+		return;
+	}
 	nm_st_cb_wstate(scb, SCB_M_NOREF);
 	nm_st_extra_dequeue(scb_kring(scb), scb_slot(scb));
 }

sys/dev/netmap/netmap_kern.h
@@ -1188,6 +1188,7 @@ struct nm_st_cb {
 	struct nm_ubuf_info ui; /* ctx keeps kring and desc keeps slot */
 #define SCB_M_MAGIC		0x12345600	/* XXX do better */
 #define SCB_M_MAGIC_MASK	0xffffff00	/* XXX do better */
+#define SCB_GONE		0x00000001
 	uint32_t flags;
 	uint32_t next;
 } __attribute__((__packed__)); /* 32 byte */
@@ -1207,6 +1208,24 @@ static inline int
 nm_st_cb_valid(struct nm_st_cb *scb)
 {
 	return ((scb->flags & SCB_M_MAGIC_MASK) == SCB_M_MAGIC);
+}
+
+static inline int
+nm_st_cb_gone(struct nm_st_cb *scb)
+{
+	return !!(scb->flags & SCB_GONE);
+}
+
+static inline void
+nm_st_cb_set_gone(struct nm_st_cb *scb)
+{
+	scb->flags |= SCB_GONE;
+}
+
+static inline void
+nm_st_cb_clr_gone(struct nm_st_cb *scb)
+{
+	scb->flags &= ~SCB_GONE;
 }
 
 static inline int